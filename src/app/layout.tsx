b6c459733ccb2ca6b1f825cf87683a5e45b7bb9f
foregato <arthurforegato@hotmail.com>
2026-07-30 16:27:27 -0300

    ele da problema esse ./globa mas -e util

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Fraunces, Inter, Space_Mono } from "next/font/google";
+import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 
